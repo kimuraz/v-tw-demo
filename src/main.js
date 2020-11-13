24b46b4ee948b35b5c45dd55ba3de4a7f8dfa76d
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-11-14 00:36:53 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import VTW from "v-tw-directives";
+
+Vue.config.productionTip = false;
+
+Vue.use(VTW, {
+  tw: {
+    button: {
+      classes: "bg-teal-300 hover:bg-teal-400 text-white py-2 px-3",
+    },
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
